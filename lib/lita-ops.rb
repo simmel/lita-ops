d4fd2e3fab8f3b9ba4b411bf642ee5a0e1781a8b
Simon Lundström <simmel@soy.se>
2015-06-25 21:47:02 +0200

    Only run when the IRC adapter is being used

--- lib/lita-ops.rb
+++ lib/lita-ops.rb
@@ -3,6 +3,14 @@ require "lita"
 module Lita
   module Handlers
     class Ops < Handler
+
+      on(:connected) do |payload|
+        if robot.send(:adapter).class != Lita::Adapters::IRC
+          log.fatal "Only the IRC adapter is supported."
+          exit
+        end
+      end
+
     end
 
     Lita.register_handler(Ops)
