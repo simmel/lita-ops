629119c32fb10b00522d77f15dfbc7dd4755024d
Simon Lundström <simmel@soy.se>
2015-06-25 21:22:52 +0200

    Simplify the spec

--- spec/lita/handlers/ops_spec.rb
+++
@@ -1,4 +0,0 @@
-require "spec_helper"
-
-describe Lita::Handlers::Ops, lita_handler: true do
-end
--- spec/ops_spec.rb
+++ spec/ops_spec.rb
@@ -4,3 +4,6 @@ require "lita/rspec"
 # A compatibility mode is provided for older plugins upgrading from Lita 3. Since this plugin
 # was generated with Lita 4, the compatibility mode should be left disabled.
 Lita.version_3_compatibility_mode = false
+
+describe Lita::Handlers::Ops, lita_handler: true do
+end
