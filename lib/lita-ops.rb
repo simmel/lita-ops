4d56fdce3910a833a9e977fe7d765df18d89e291
Simon Lundström <simmel@soy.se>
2017-02-09 15:03:19 +0100

    Add config logic

--- lib/lita-ops.rb
+++ lib/lita-ops.rb
@@ -5,6 +5,8 @@ module Lita
   module Handlers
     class Ops < Handler
 
+      config :should_be_oped, type: Hash, required: true
+
       on(:connected) do |payload|
         if robot.send(:adapter).class != Lita::Adapters::IRC
           log.fatal "Only the IRC adapter is supported."
@@ -15,7 +17,18 @@ module Lita
       on :user_joined_room, :op
 
       def op(payload)
-        payload[:room].op(robot, nick)
+        cinch = robot.send(:adapter).cinch
+        nick = cinch.user_list.find_ensured(payload[:user].name)
+        channel = payload[:room].name
+
+        if config.should_be_oped.include?(channel)
+          config.should_be_oped[channel].each do |u|
+            if nick.mask.eql? Cinch::Mask.new(u)
+              log.debug "#{u} matched!"
+              payload[:room].op(robot, nick)
+            end
+          end
+        end
       end
 
     end
