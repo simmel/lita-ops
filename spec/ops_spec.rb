6c37876dd94b02025fa414e4f5a9ec9dc9764a9f
Simon Lundström <simmel@soy.se>
2017-02-13 13:03:22 +0100

    Add a positive and a negative test

--- spec/ops_spec.rb
+++ spec/ops_spec.rb
@@ -1,4 +1,5 @@
 require "lita-ops"
+require "lita-irc"
 require "lita/rspec"
 
 # A compatibility mode is provided for older plugins upgrading from Lita 3. Since this plugin
@@ -6,4 +7,30 @@ require "lita/rspec"
 Lita.version_3_compatibility_mode = false
 
 describe Lita::Handlers::Ops, lita_handler: true do
+
+  let(:allowed_nick) { double("Cinch::User", nick: "simmel", mask: Cinch::Mask.new("simmel!~simmel@localhost")) }
+  let(:denied_nick) { double("Cinch::User", nick: "evil", mask: Cinch::Mask.new("evil!~evil@localhost")) }
+  let(:channel) { "#marvintesting" }
+
+  before do
+    registry.configure do |config|
+      config.handlers.ops.should_be_oped = {
+        "#marvintesting" => %w(
+          Lita!~simmel@localhost
+          simmel!~simmel@localhost
+        )
+      }
+    end
+  end
+
+  describe "#should_be_oped?" do
+    it "allowed user" do
+      expect(subject.should_be_oped?(channel, allowed_nick)).to be true
+    end
+
+    it "denied user" do
+      expect(subject.should_be_oped?(channel, denied_nick)).to be false
+    end
+
+  end
 end
